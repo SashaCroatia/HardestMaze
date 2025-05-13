dbf2c2c975f10a09b123519a8e344582ffc980ab
Alexander Berliner <aiberliner@arizona.edu>
2025-05-13 03:10:41 -0700

    final final edits

--- comparisons.py
+++ comparisons.py
@@ -7,34 +7,36 @@ import numpy as np
 
 #Setup
 #=================
-#INSTRUCTORS: if you want to run all comparisons, change value to True (warning: may take a while)
+#INSTRUCTORS: if you want to run all comparisons, change value below to True (warning: may take a while)
 perform_all = False
 if perform_all == True:
-  creations = 30 #number of maze creations per method used in my experiments
+  #what's used in my experiments
+  creations = 30 #number of observations in each experiment
+  dimension = 6 #dimension of mazes used in my experiments
 else:
-  creations = 1 #for testing purposes only
+  #subset: for testing purposes only (feel free to modify)
+  creations = 2
+  dimension = 6
 
-methods = ['dfs', 'ucs', 'greedy', 'astar']
-
-print("Experiment 1 - nodepath")
-print("=================")
+print("Experiment 1 - Longest Path")
+print("===========================")
 metric = 'nodepath'
 p1 = 0.86
 p2 = 0.1
 max_iter = 400
 method_created = []
-for method in methods:
+for method in ['dfs', 'ucs', 'greedy', 'astar']:
   created = []
   np.random.seed(10)
   for creation in range(creations):
     #Define dimensions of empty grid
-    Maze = prb.Maze(6,6)
+    Maze = prb.Maze(dimension,dimension)
 
     #Generate starting maze
     maze = Maze.gen_maze_adversarial(None, metric, p1, p2, method, True, max_iter, 2, False, False)
 
     #Modify that maze
-    maze = Maze.gen_maze_adversarial(maze, metric, 1, 0.05, method, True, max_iter, 6, False, False) #30
+    maze = Maze.gen_maze_adversarial(maze, metric, 1, 0.05, method, True, max_iter, dimension, False, False) #30
 
     #Solve that maze
     result = Maze.solve_maze(maze, method, False, 0)
@@ -48,28 +50,29 @@ for method in methods:
   method_created.append(np.array([means, stds]))
 
 method_created = np.array(method_created)
+print("\nSummary statistics: 4 rows for each method. 2 subrows = avg, std of tuple data")
 print(method_created)
 
 
-print("\n\nExperiment 2 - node")
-print("=================")
+print("\n\nExperiment 2 - Most Nodes Visited")
+print("===========================")
 metric = 'node'
 p1 = 0.86
 p2 = 0.1
 max_iter = 200
 method_created = []
-for method in methods:
+for method in ['dfs', 'ucs', 'greedy', 'astar']:
   created = []
   np.random.seed(10)
   for creation in range(creations):
     #Define dimensions of empty grid
-    Maze = prb.Maze(6,6)
+    Maze = prb.Maze(dimension,dimension)
 
     #Generate starting maze
     maze = Maze.gen_maze_adversarial(None, metric, p1, p2, method, True, max_iter, 2, False, False)
 
     #Modify that maze
-    maze = Maze.gen_maze_adversarial(maze, metric, 1, 0.05, method, True, max_iter, 6, False, False) #30
+    maze = Maze.gen_maze_adversarial(maze, metric, 1, 0.05, method, True, max_iter, dimension, False, False) #30
 
     #Solve that maze
     result = Maze.solve_maze(maze, method, False, 0)
@@ -83,28 +86,29 @@ for method in methods:
   method_created.append(np.array([means, stds]))
 
 method_created = np.array(method_created)
+print("\nSummary statistics: 4 rows for each method. 2 subrows = avg, std of tuple data")
 print(method_created)
 
 
-print("\n\nExperiment 3 - deadend")
-print("=================")
+print("\n\nExperiment 3 - Most Dead-Ends")
+print("===========================")
 metric = 'deadend'
 p1 = 0.92
 p2 = 0.02
 max_iter = 200
 method_created = []
-for method in methods:
+for method in ['dfs', 'ucs', 'greedy', 'astar']:
   created = []
   np.random.seed(10)
   for creation in range(creations):
     #Define dimensions of empty grid
-    Maze = prb.Maze(6,6)
+    Maze = prb.Maze(dimension,dimension)
 
     #Generate starting maze
     maze = Maze.gen_maze_adversarial(None, metric, p1, p2, method, True, max_iter, 2, False, False)
 
     #Modify that maze
-    maze = Maze.gen_maze_adversarial(maze, metric, 1, 0.05, method, True, max_iter, 6, False, False) #30
+    maze = Maze.gen_maze_adversarial(maze, metric, 1, 0.05, method, True, max_iter, dimension, False, False) #30
 
     #Solve that maze
     result = Maze.solve_maze(maze, method, False, 0)
@@ -118,4 +122,5 @@ for method in methods:
   method_created.append(np.array([means, stds]))
 
 method_created = np.array(method_created)
+print("\nSummary statistics: 4 rows for each method. 2 subrows = avg, std of tuple data")
 print(method_created)
--- demo.py
+++ demo.py
@@ -11,40 +11,45 @@ algorithm and one player uses another. If your problem is snake, you might show 
 (whichever comes first).
 '''
 import problems as prb
+import numpy as np
 
-#To be set by User:
+#To be set by User/Instructor:
 #------------------------
-#Define dimensions of empty grid
-Maze = prb.Maze(5,5) #small maze for computational speed reasons, as this is a demo
+#Define dimensions of empty grid and max_iter for maze gen
+dimension = 5 #small maze for computational speed reasons, as this is a demo. (in report used 6).
+max_iter = 200 #for demo purposes. (in report used 400).
+Maze = prb.Maze(dimension, dimension)
 #------------------------
 
-for metric in ['nodepath','node','deadend']:
-    for method in ['astar','dfs','greedy','ucs']:
+np.random.seed(6)
+for metric in ['nodepath','node','deadend']: #path to solution, nodes in search space, num deadends
+    for method in ['dfs', 'ucs', 'greedy', 'astar']: #search algo
         if metric == 'nodepath':
-            p1 = 0.86
+            p1 = 0.85
             p2 = 0.1
-            max_iter = 400
+            max_iter = max_iter
         elif metric == 'node':
             p1 = 0.86
             p2 = 0.1
-            max_iter = 200
+            max_iter = max_iter//2
         elif metric == 'deadend':
             p1 = 0.92
             p2 = 0.02
-            max_iter = 200
+            max_iter = max_iter//2
         else:
             #metric == 'path'
             p1 = 0.85
             p2 = 0.15
-            max_iter = 200
+            max_iter = max_iter//2
 
         print("-------------------------")
         #Generate starting maze
         maze = Maze.gen_maze_adversarial(None, metric, p1, p2, method, True, max_iter, 1, False, True)
 
         #Modify that maze
-        maze = Maze.gen_maze_adversarial(maze, metric, 1, 0.05, method, True, max_iter, 3, False, True)
+        maze = Maze.gen_maze_adversarial(maze, metric, 1, 0.05, method, True, max_iter, dimension, False, True)
 
         #Solve that maze
         print(f"metric = {metric} | method = {method} | path len, nodes explored, dead ends = {Maze.solve_maze(maze, method, True, 1, 11, method)}")
         print("-------------------------")
+print(f"{Maze.solve_maze(maze, method, True, 0, 11, method)}")
